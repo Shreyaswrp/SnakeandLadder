6d08ae75d0d3c8ef8cd2d30ac6ee53f0b77e280f
Shreya Swaroop <shreyaswrp@gmail.com>
2020-08-01 08:36:45 +0530

    Added:to repeat till the position reaches 100

--- SnakeAndLadder.sh
+++ SnakeAndLadder.sh
@@ -7,20 +7,22 @@ START_POSITION=0
 player=1
 position=0
 
+while [ $position -le 100 ]
+do
 #rolling a die to get the result using RANDOM
-dieresult=$((RANDOM % 6 + 1))
+dieResult=$((RANDOM % 6 + 1))
 
 #checking for options to proceed using RANDOM
 options=$((RANDOM % 3))
-if [[ $options == 0 ]]
-	then
-		echo "No play"
-	elif [[ $options == 1 ]]
-		then
-			echo "Snake"
-			position=$((position - dieresult))
-	else
-		echo "Ladder"
-		positin=$((position + dieresult ))
-fi
-
+	case $options in
+		0)position=$((position));;
+		1)position=$((position+dieResult));;
+		2)result=$((position-dieResult))
+			if [ $result -gt 0 ]
+			then
+				position=$((position-dieResult))
+			else
+				position=$position
+			fi;;
+	esac
+done
