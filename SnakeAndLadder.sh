94fe06bfb858e766a1b038d9c96ad23a7b453e4a
Shreya Swaroop <shreyaswrp@gmail.com>
2020-08-01 08:18:35 +0530

    Added:condition to proceed in the game

--- SnakeAndLadder.sh
+++ SnakeAndLadder.sh
@@ -5,8 +5,22 @@ START_POSITION=0
 
 #variables
 player=1
+position=0
 
 #rolling a die to get the result using RANDOM
 dieresult=$((RANDOM % 6 + 1))
 
+#checking for options to proceed using RANDOM
+options=$((RANDOM % 3))
+if [[ $options == 0 ]]
+	then
+		echo "No play"
+	elif [[ $options == 1 ]]
+		then
+			echo "Snake"
+			position=$((position - dieresult))
+	else
+		echo "Ladder"
+		positin=$((position + dieresult ))
+fi
 
