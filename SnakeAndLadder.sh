ef4e026bcac53697991e15ee2f92b2ed3ebe4bb4
Shreya Swaroop <shreyaswrp@gmail.com>
2020-08-02 16:44:53 +0530

    Added:to get count of die roll and position after every dieroll

--- SnakeAndLadder.sh
+++ SnakeAndLadder.sh
@@ -7,24 +7,33 @@ START_POSITION=0
 player=1
 position=0
 
+#to store the position after every dice roll
+declare -a diceArray
+diceRoll=0
+
 while [ $position -lt 100 ]
 do
 	#rolling a die to get the result using RANDOM
 	dieResult=$((RANDOM % 6 + 1))
 
+	#to get dice roll count
+	((diceRoll++))
 	#checking for options to proceed using RANDOM
 	options=$((RANDOM % 3))
 
 	case $options in
 
-		0)position=$((position));;
+		0)position=$((position))
+			    diceArray[$diceRoll]=$position;;
 
 		1)result=$((position+dieResult))
 			if [ $result -le 100 ]
 			then
 				position=$((position+dieResult))
+				diceArray[$diceRoll]=$position
 			else
 				position=$position
+				diceArray[$diceRoll]=$position
 			fi
 			;;
 
@@ -32,13 +41,18 @@ do
 			if [ $result -gt 0 ]
 			then
 				position=$((position-dieResult))
+				diceArray[$diceRoll]=$position
 			else
 				position=$position
+				diceArray[$diceRoll]=$position
 			fi;;
 
 
 	esac
 
 done
+
 echo $position
+echo $diceRoll times dice was played to win the game.
+echo Positions after every dice roll ${diceArray[@]}
 
